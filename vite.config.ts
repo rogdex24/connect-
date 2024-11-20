9557c9f4a10fdf079d2976bb2fab53bb1a10d513
Abu Talha <rogdex24@gmail.com>
2024-11-21 03:35:03 +0530

    fix: gh-pages basepath issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.ENV === "dev" ? "/" : "/connect-plus/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
